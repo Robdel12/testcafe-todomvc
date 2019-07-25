f2ec9b1ec40c377b2dc8f70cb1cfc9cdcf2a0d09
Robert DeLuca <robertdeluca19@gmail.com>
2019-07-25 10:29:16 -0500

    Update SDK to latest agent version

--- tests/utils.js
+++ tests/utils.js
@@ -1,8 +1,24 @@
 import fs from "fs";
 import { ClientFunction } from "testcafe";
-import { agentJsFilename } from "@percy/agent";
+const {
+  agentJsFilename,
+  postSnapshot
+} = require("@percy/agent/dist/utils/sdk-utils");
 
 export class Percy {
+  async postDomSnapshot(name, domSnapshot, url, options) {
+    const postSuccess = await postSnapshot({
+      name,
+      url,
+      domSnapshot,
+      clientInfo: clientInfo(),
+      ...options
+    });
+    if (!postSuccess) {
+      console.log(`[percy] Error posting snapshot to agent`);
+    }
+  }
+
   async percySnapshot(snapshotName, snapshotOptions) {
     let agentFileContents = fs.readFileSync(agentJsFilename()).toString();
     let loadScriptFile = ClientFunction(
